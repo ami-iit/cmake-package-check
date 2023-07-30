dda3728e2a1407b63e38b10f0f1e9ba449bba900
Silvio Traversaro <silvio.traversaro@iit.it>
2023-07-30 17:53:06 +0200

    Add first

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # cmake-package-check
-Utility to check if a CMake package exists.
+
+Command line utility to check if a CMake package exists.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install cmake-package-check.
+
+```bash
+python -m pip install git+https://github.com/ami-iit/cmake-package-check
+```
+
+The package assumes that the following tools used by CMake are already installed in the system, i.e.:
+* C and C++ compiler
+* CMake
+* Ninja
+* pkg-config
+
+
+## Usage
+
+### Check if a package exists
+
+To check if the CMake package `fmt` is installed and can be found by the CMake, run:
+```bash
+cmake-package-check fmt
+```
+
+The command output will end with `cmake-package-check: SUCCESS.` if the package can be found, and with `cmake-package-check: FAILURE.` . In case of success the return value of the command will be 0, while it will be 1 if the package can't be found. This permits to use the command in the context of continuous integration scripts.
+
+**Note: on Windows, the command needs to run in Developer Command Prompt or Developer Powershell.**
+
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[BSD-3-Clause](https://spdx.org/licenses/BSD-3-Clause.html)
---
+++ src/cmake_package_check/cmake_package_check.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import subprocess
+import tempfile
+from jinja2 import Template, Environment, PackageLoader
+import argparse
+
+def cmake_package_check(CMakePackageNames):
+    temp_dir = tempfile.mkdtemp()
+
+    try:
+        # Prepare context for Jinja2 template
+        context = {
+            'CMakePackageNames': CMakePackageNames
+        }
+
+        # Render CMakeLists.txt from the Jinja2 template
+        environment = Environment(loader=PackageLoader("cmake_package_check"))
+        template = environment.get_template("CMakeLists.txt.in")
+        cmake_content = template.render(context)
+
+        # Write the rendered CMakeLists.txt to the temporary directory
+        os.chdir(temp_dir)
+        with open('CMakeLists.txt', 'w') as f:
+            f.write(cmake_content)
+
+        print("===================================")
+        print("=== Generated CMakeLists.txt file: ")
+        print("===================================")
+        print(cmake_content)
+
+        # Run CMake in the temporary directory
+        print("===================================")
+        print("=== CMake configure output:        ")
+        print("===================================")
+        subprocess.run(['cmake','-S.', '-B.'], cwd=temp_dir, check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+        return False
+
+    # If all is ok, return true
+    return True
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Utility to check if a CMake package exists.")
+    parser.add_argument("CMakePackageNames", metavar="CMakePackageNames", type=str, nargs="+", help="Names of the cmake packages to check the existence")
+
+    args = parser.parse_args()
+    result = cmake_package_check(args.CMakePackageNames)
+
+    print("===================================")
+    print("=== Result:")
+    print("===================================")
+
+    if(result):
+        print("cmake-package-check: SUCCESS.")
+        sys.exit(0)
+    else:
+        print("cmake-package-check: FAILURE.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
